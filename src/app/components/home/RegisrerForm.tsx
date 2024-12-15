d0f037eb9103fe789154604c82f7bcbcaca9e13b
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2024-12-15 19:52:25 +0200

    add inputs

--- src/app/components/home/RegisrerForm.tsx
+++ src/app/components/home/RegisrerForm.tsx
@@ -1,35 +1,46 @@
 'use client';
 import RegisterInputs from "@/app/UI/Inputs/RegisterInput";
 import { title } from "process";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 export default function RegisterForm() {
-    const formPassword = /^(?=.*[a-zA-Z])(?=.*[*!#&])[A-Za-z0-9*!#&]{6,}$/
+    const formPassword = /^(?=.*[a-zA-Z])(?=.*[*!#&])[A-Za-z0-9*!#&]{6,}$/;
+    const formEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const formPhone = /^\+380\d{9}$/;
+
     const [isEmptyPassword, setIsEmptyPassword] = useState(false);
+    const [isEmptyName, setIsEmptyName] = useState(false);
+    const [isEmptyEmail, setIsEmptyEmail] = useState(false)
+    const [isEmptyRepeatPassword, setIsEmptyRepeatPassword] = useState(false);
+    const [isEmptyPhone, setIsEmptyPhone] = useState(false);
 
     const [password, setPassword] = useState('');
+    const [name, srtName] = useState('');
+    const [email, setEmail] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+    const [phone, setPhone] = useState('');
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
        const {name, value,} = e.currentTarget;
         switch (name) {
-          
-        //    case 'email':
-            // setEmail(value);
-            // break;
+           
+            case 'name':
+            srtName(value);
+            break;
+
+           case 'email':
+            setEmail(value);
+            break;
             case 'password':
                 setPassword(value);
             break;
+             
+            case 'repeatPassword':
+            setRepeatPassword(value);
+            break;
             
-            // case 'emailTwo':
-            // setEmailTwo(value);
-            // break;
-            
-            // case 'code':
-            // setCode(value);
-            // break;
-            
-            //  case 'newPassword':
-            // SetNewPassword(value);
-            // break;
+             case 'phone':
+            setPhone(value);
+            break;
         
            default:
            return;  
@@ -39,9 +50,29 @@ export default function RegisterForm() {
 
    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
        e.preventDefault();
-       if (password === '' || password.length < 6 || !formPassword.test(password)) {
-           
+       if (password === '' || password.length < 6 || !formPassword.test(password)
+       ) {  
            setIsEmptyPassword(true);
+           return;
+       }
+
+       if (name === '') {
+           setIsEmptyName(true);
+           return;
+       }
+
+       if (email === '' || !formEmail.test(email)) {
+           setIsEmptyEmail(true);
+           return;
+       }
+
+       if (repeatPassword !== password) {
+           setIsEmptyRepeatPassword(true);
+           return;
+       }
+
+       if (phone === '' || !formPhone.test(phone)) {
+           setIsEmptyPhone(true);
            return;
        }
        
@@ -50,9 +81,25 @@ export default function RegisterForm() {
     return (
         <form onSubmit={handleSubmit} >
 
-            <RegisterInputs title = 'Пароль' name = 'password' type = 'password' placeholder = 'Very#5' 
+            <RegisterInputs title = 'Імʼя*' name = 'name' type = 'text' placeholder = 'Іван' 
+                message='Поле не може залишатись постим.'
+                value={name} isError={isEmptyName} change={handleChange} />
+            
+            <RegisterInputs title = 'E-mail*' name = 'email' type = 'email' placeholder = 'email@org.ua' 
+                message='Потрібно ввести E-mail у наступному форматі: email@org.ua.'
+                value={email} isError={isEmptyEmail} change={handleChange} />
+
+            <RegisterInputs title = 'Пароль*' name = 'password' type = 'password' placeholder = 'Very#5' 
                 message='Пароль має містити щонайменше 6 символів, включаючи літери та спеціальні знаки (, #, & тощо)'
                 value={password} isError={isEmptyPassword} change={handleChange} />
+            
+             <RegisterInputs title = 'Повторіть пароль*' name = 'repeatPassword' type = 'password' placeholder = 'Very#5' 
+                message='Потрібно ввести одинакові паролі'
+                value={repeatPassword} isError={isEmptyRepeatPassword} change={handleChange} />
+            
+            <RegisterInputs title = 'Введіть номер телефону*' name = 'phone' type = 'phone' placeholder = '+380670000000' 
+                message='Потрібно ввести телефон у наступному форматі: +380670000000'
+                value={phone} isError={isEmptyPhone} change={handleChange} />
             
             <button type="submit">Зареєструватися</button>
         </form>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { Metadata } from "next";
 import fon from '../assets/bg-img.png'; 
 import RegisterForm from '../components/home/RegisrerForm';
+import ButtonGoogle from '../UI/Buttons/ButtonGoogle';
 export const metadata: Metadata = {
   title: "Реєстрація",
   description: "Сторінка реєстрації",
@@ -15,6 +16,7 @@ export default function Register() {
                 <div className='w-[501px] bg-white shadow-base px-6 py-10 rounded-[24px]'>
                     <p className='text-center text-3xl text-black font-medium mb-6'>Вхід
                         <span className='font-bold text-4xl ml-6'>Реєстрація</span></p>
+                    <ButtonGoogle/>
                     <RegisterForm/> 
                 </div> 
 
