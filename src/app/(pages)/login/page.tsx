c42ede648c356052576424e9769548ead52f2e37
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2024-12-16 18:43:52 +0200

    create login form

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 import { Metadata } from "next";
-import fon from '../assets/bg-img.png'; 
-import RegisterForm from '../components/Register/RegisterForm';
-import ButtonGoogle from '../UI/Buttons/ButtonGoogle';
+import fon from '../../assets/bg-img.png'; 
+import Login from '@/app/components/Login/Login';
+import ButtonGoogle from '../../UI/Buttons/ButtonGoogle';
 export const metadata: Metadata = {
   title: "Реєстрація",
   description: "Сторінка реєстрації",
@@ -10,12 +10,12 @@ export const metadata: Metadata = {
 
 export default function Register() {
     return (
-        <div className="w-full bg-gray-0 pt-[125px] relative overflow-hidden">
-      <div className='w-[1249px] ml-auto mr-auto container pb-7'>
+        <div className="w-full bg-gray-0 pt-[215px] relative overflow-hidden">
+      <div className='w-[1249px] ml-auto mr-auto container pb-[215px]'>
                 
                 <div className='w-[501px] bg-white shadow-base px-6 py-10 rounded-[24px]'>
-                    <p className='text-center text-3xl text-black font-normal mb-6'>Вхід
-                    <span className='font-bold text-4xl ml-6'>Реєстрація</span></p>
+                    <p className='text-center font-bold text-4xl mb-6'>Вхід
+                    <span className='text-3xl text-black font-normal  ml-6'>Реєстрація</span></p>
                     
                     <ButtonGoogle />
                     <div className='flex items-center mb-6 gap-4'>
@@ -23,8 +23,7 @@ export default function Register() {
                     <p className='text-xl text-gray-20 text-center font-normal'>або</p>
                     <div className='w-[199px] h-[1px] bg-gray-20 '></div>
                     </div>
-
-                    <RegisterForm/> 
+                    <Login/>
                 </div> 
 
                
---
+++ src/app/(pages)/register/page.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import { Metadata } from "next";
+import fon from '../../assets/bg-img.png'; 
+import RegisterForm from '../../components/Register/RegisterForm';
+import ButtonGoogle from '../../UI/Buttons/ButtonGoogle';
+export const metadata: Metadata = {
+  title: "Реєстрація",
+  description: "Сторінка реєстрації",
+};
+
+export default function Register() {
+    return (
+        <div className="w-full bg-gray-0 pt-[125px] relative overflow-hidden">
+      <div className='w-[1249px] ml-auto mr-auto container pb-7'>
+                
+                <div className='w-[501px] bg-white shadow-base px-6 py-10 rounded-[24px]'>
+                    <p className='text-center text-3xl text-black font-normal mb-6'>Вхід
+                    <span className='font-bold text-4xl ml-6'>Реєстрація</span></p>
+                    
+                    <ButtonGoogle />
+                    <div className='flex items-center mb-6 gap-4'>
+                    <div className='w-[199px] h-[1px] bg-gray-20 '></div>
+                    <p className='text-xl text-gray-20 text-center font-normal'>або</p>
+                    <div className='w-[199px] h-[1px] bg-gray-20 '></div>
+                    </div>
+
+                    <RegisterForm/> 
+                </div> 
+
+               
+            </div>
+            <div className='absolute right-0 top-0'>
+                 <Image
+                src={fon} 
+                alt="fon" 
+                width={609}                 
+                height={679} 
+                quality={100}
+                priority      
+                    /> 
+           </div>
+        </div>
+    )
+}
+
